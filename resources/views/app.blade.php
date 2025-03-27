a2c8735071a2da85972c49edebd172f46d5de339
Soymilk71 <bobarends69@gmail.com>
2025-03-27 13:43:01 +0100

    added app template with vite

---
+++ resources/views/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="strippenkaart" content="klanten pagina voor uren">
+    <title>Strippenkaart</title>
+</head>
+<header>
+<div>
+    <div>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/klanten">Login</a></li>
+                <li><a href="/uren">Register</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+<body>
+    @yield('content')
+</body>
+<footer>
+    <div>
+        <p>&copy; 2020 Strippenkaart</p>
+    </div>
+</footer>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('app')
+
+@section('content')
+    <div class="container">
+        <div class="content">
+            <div class="title"></div>
+            <div class="quote">HOI</div>
+        </div>
+    </div>
+@endsection
