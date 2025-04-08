6d4c20ca24059a26955d4c2119b616a5c78fec90
Soymilk71 <bobarends69@gmail.com>
2025-04-08 10:09:26 +0200

    made migrations for now

---
+++ database/migrations/2025_04_07_095150_strippen.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('strippen', function(Blueprint $table){
+            $table->id();
+            $table->integer('klant_id');
+            $table->date('date');
+            $table->string('type');
+            $table->integer('uren');
+            $table->integer('uren_na_submit');
+            $table->integer('titel')->nullable();
+            $table->string('omschrijving')->nullable();
+            $table->string('jira_link')->nullable();
+            $table->timestamps();
+        });
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
